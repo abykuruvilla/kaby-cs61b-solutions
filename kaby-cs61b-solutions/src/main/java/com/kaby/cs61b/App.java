f42c16d23ac1cbb8b56042e6b135d6e8d1f4eb8b
Aby Kuruvilla <abykuruvilla@hotmail.com>
2015-09-05 15:44:26 -0400

    Lab1 initial commit

---
+++ kaby-cs61b-solutions/src/main/java/com/kaby/cs61b/App.java
@@ -0,0 +1,13 @@
+package com.kaby.cs61b;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
